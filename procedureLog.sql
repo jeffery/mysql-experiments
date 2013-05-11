1bf0d4b443ab4fecbf68214f9d1b7c1f8455eccf
Jeffery Fernandez <jeffery@fernandez.net.au>
2013-05-11 14:39:00 +1000

    Consolidated procedureLog and setupProcedureLog into procedureLog

--- procedureLog.sql
+++ procedureLog.sql
@@ -1,31 +1,5 @@
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS setupProcedureLog //
-CREATE PROCEDURE setupProcedureLog( )
-	BEGIN
-		DECLARE procedureLogExists INT DEFAULT 0;
-
-		SELECT
-			count( * )
-		INTO procedureLogExists
-		FROM information_schema.tables
-		WHERE table_schema = database( ) AND table_name = 'procedureLog';
-
-		IF procedureLogExists = 0
-		THEN
-			CREATE TABLE procedureLog (
-				id           INT(2) UNSIGNED NOT NULL AUTO_INCREMENT,
-				logTime      DATETIME,
-				connectionId INT             NOT NULL DEFAULT 0,
-				logMessage   VARCHAR(20000),
-				PRIMARY KEY (id)
-			);
-		END IF;
-		
-	END
-//
-
-
 DROP PROCEDURE IF EXISTS procedureLog //
 CREATE PROCEDURE procedureLog( IN logMsg VARCHAR(20000) )
 	BEGIN
@@ -34,21 +8,37 @@ CREATE PROCEDURE procedureLog( IN logMsg VARCHAR(20000) )
 
 		DECLARE CONTINUE HANDLER FOR SQLSTATE '42S02' -- Table not found
 		BEGIN
-			CALL setupProcedureLog;
+			DECLARE procedureLogExists INT DEFAULT 0;
+			SELECT
+				count( * )
+			INTO procedureLogExists
+			FROM information_schema.tables
+			WHERE table_schema = database( ) AND table_name = 'procedureLog';
+
+			IF procedureLogExists = 0
+			THEN
+				CREATE TABLE procedureLog (
+					id           INT(2) UNSIGNED NOT NULL AUTO_INCREMENT,
+					logTime      DATETIME,
+					connectionId INT             NOT NULL DEFAULT 0,
+					logMessage   VARCHAR(20000),
+					PRIMARY KEY (id)
+				);
+			END IF;
 		END;
 
 		SELECT
 			count( * )
 		INTO checkConnectionId
 		FROM procedureLog
-		WHERE connectionId != CONNECTION_ID();
+		WHERE connectionId != CONNECTION_ID( );
 
 		IF checkConnectionId > 0
 		THEN
 			TRUNCATE TABLE procedureLog;
 		END IF;
 
-		INSERT INTO procedureLog (logTime, connectionId, logMessage) VALUES ( NOW(), CONNECTION_ID(), logMsg );
+		INSERT INTO procedureLog (logTime, connectionId, logMessage) VALUES ( NOW( ), CONNECTION_ID( ), logMsg );
 	END
 //
 
